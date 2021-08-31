b1f33a11ac7038641296b6a744b40ae15814da8d
CLE <clenzdevapp@gmail.com>
2021-08-31 10:51:57 +0200

    v1.0

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "https://tinder-clone-backend-4.herokuapp.com/",
-  baseURL: "https://tinder-clone-4.herokuapp.com/"
+  // baseURL: "http://localhost:8001"
+  baseURL: "https://tinder-clone-backend-4.herokuapp.com/",
+  // baseURL: "https://tinder-clone-4.herokuapp.com/"
 });
 
 export default instance;
